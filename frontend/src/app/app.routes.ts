854af655513e75bf4c182b0392368fbf222ae2fe
JeffersonCMP1 <jefferson.mamani0061@alumnos.udg.mx>
2025-11-11 00:20:16 -0500

    J: agregué código de olvidar y recuperar contraseña

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+//nuevas rutas
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
 export const routes: Routes = [
     { path: "", component: LoginComponent },
     { path: "home", component: HomeComponent },
+    { path: 'forgot-password', component: ForgotPasswordComponent },
+    { path: 'reset-password/:token', component: ResetPasswordComponent },
     // { path: "layout", component: LayoutComponent }
     {
         path: "layout", component: LayoutComponent,
